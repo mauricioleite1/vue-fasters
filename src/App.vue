0be09ca4ca939296e4087c346296f223c5fee626
Maurício Leite <mauricioleite1@gmail.com>
2022-04-12 11:20:54 -0300

    Criação do Highlighted Comment

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <the-header></the-header>
   <the-hero></the-hero>
   <about-section></about-section>
+  <highlighted-comment></highlighted-comment>
   <the-hero></the-hero>
 </template>
 
@@ -9,6 +10,7 @@
 import TheHeader from './components/TheHeader/TheHeader.vue';
 import TheHero from './components/TheHero/TheHero.vue';
 import AboutSection from './components/AboutSection/AboutSection.vue';
+import HighlightedComment from './components/HighlightedComment/HighlightedComment.vue';
 
 export default {
   name: 'App',
@@ -16,6 +18,7 @@ export default {
     TheHeader,
     TheHero,
     AboutSection,
+    HighlightedComment,
   },
 };
 </script>
---
+++ src/components/HighlightedComment/HighlightedComment.vue
@@ -0,0 +1,19 @@
+<template>
+  <section :class="[styles.container]">
+    Highlight
+    <img src="@/assets/highlighted-comment-1.png" alt="" />
+  </section>
+</template>
+
+<script>
+import styles from './HighlightedComment.module.scss';
+
+export default {
+  name: 'HighlightedComment',
+  data() {
+    return {
+      styles,
+    };
+  },
+};
+</script>
