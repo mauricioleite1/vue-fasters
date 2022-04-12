0b948d4b7a4964b778f215f4e1e1288fd183cc89
Maurício Leite <mauricioleite1@gmail.com>
2022-04-12 10:31:57 -0300

    Implementação do componente About (AboutSection)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <the-header></the-header>
   <the-hero></the-hero>
+  <about-section></about-section>
+  <the-hero></the-hero>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader/TheHeader.vue';
 import TheHero from './components/TheHero/TheHero.vue';
+import AboutSection from './components/AboutSection/AboutSection.vue';
 
 export default {
   name: 'App',
   components: {
     TheHeader,
     TheHero,
+    AboutSection,
   },
 };
 </script>
---
+++ src/components/AboutSection/AboutSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <section :class="[styles.container]">
+    <div :class="[styles.wrapper]">
+      <img src="@/assets/renee-photo.jpg" alt="Renee Trajar frontal photo" />
+      <div :class="[styles.info]">
+        <h1>Renee Trajar<hr></h1>
+        <p>{{ text.aboutFirstPart }}</p>
+        <p>{{ text.aboutSecondPart }}</p>
+        <p>{{ text.aboutCitation }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import styles from './AboutSection.module.scss';
+import text from '../../utils/data';
+
+export default {
+  name: 'AboutSection',
+  data() {
+    return {
+      styles,
+      text,
+    };
+  },
+};
+</script>
