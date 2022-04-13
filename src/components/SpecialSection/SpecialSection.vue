b8fbee3969e3ed71acad16d28e74419794a6ca07
Maurício Leite <mauricioleite1@gmail.com>
2022-04-13 05:38:57 -0300

    Refatoração Special Section

--- src/components/SpecialSection/SpecialSection.vue
+++ src/components/SpecialSection/SpecialSection.vue
@@ -2,17 +2,19 @@
   <div :class="[styles.container]">
     <div :class="[styles.wrapper]">
       <div :class="[styles.callToActionTextAndButton]">
-        <h1>Condição Imperdível</h1>
+        <h1>{{ title }}</h1>
         <h2>Compre hoje e garanta:</h2>
+
         <p>- 12% de desconto e pagamento em até <strong>3x sem juros;</strong></p>
         <p>- Até <strong>10 meses</strong> para confeccionar sua roupa. Não se preocupe se<br>
         suas medidas aumentaram ou diminuíram, estará sempre elegante;</p>
         <p>- Condição muito especial por <strong>tempo limitado</strong></p>
         <p>- Poder presentear alguém com uma roupa de alta costura, <strong>exclusiva</strong>.</p>
-        <button>Garanta essa condição especial!</button>
+
+        <button>{{ callToActionButtonTitle }}</button>
       </div>
 
-      <img src="@/assets/specialinfo-1.jpg" alt="" />
+      <img src="@/assets/specialinfo-1.jpg" alt="Homem modelo vestindo um terno" />
     </div>
   </div>
 </template>
@@ -25,6 +27,8 @@ export default {
   data() {
     return {
       styles,
+      title: 'Condição Imperdível',
+      callToActionButtonTitle: 'Garanta essa condição especial!',
     };
   },
 };
